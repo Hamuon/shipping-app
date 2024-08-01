4e0ef576a695a2de949072029bd0a2cf121af459
Hamun <hamunjavadzadeghan@gmail.com>
2024-08-01 13:44:53 +0330

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client";
+import Bgmap from "@/components/sections/homepage1/Bgmap";
+import Brands1 from "@/components/sections/homepage1/Brands1";
+import Cta1 from "@/components/sections/homepage1/Cta1";
+import Faqs1 from "@/components/sections/homepage1/Faqs1";
+import Hero1 from "@/components/sections/homepage1/Hero1";
+import Howitwork1 from "@/components/sections/homepage1/Howitwork1";
+import Info1 from "@/components/sections/homepage1/Info1";
+import Info2 from "@/components/sections/homepage1/Info2";
+import News1 from "@/components/sections/homepage1/News1";
+import Pricing1 from "@/components/sections/homepage1/Pricing1";
+import Projects1 from "@/components/sections/homepage1/Projects1";
+import Requestquote1 from "@/components/sections/homepage1/Requestquote1";
+import Services1 from "@/components/sections/homepage1/Services1";
+import Testimonial1 from "@/components/sections/homepage1/Testimonial1";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <Hero1 />
+      <Brands1 />
+      <Services1 />
+      <Info1 />
+      <Info2 />
+      <Howitwork1 />
+      <Testimonial1 />
+      <Projects1 />
+      <Requestquote1 />
+      <Pricing1 />
+      <Faqs1 />
+      <Cta1 />
+      <News1 />
+      <Bgmap />
+    </>
   );
 }
